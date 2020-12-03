4f06275e67eb17ecbb0636ee5ebd39321da9b736
JH-92 <mbh309@gmail.com>
2020-12-04 00:06:47 +0900

    #2.0 basic frame completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
 import React from "react";
+import Movies from "./Movies";
+import axios from "axios";
+import "./App.css";
 
-function Food({fav}){
-  return <h1>I hate {fav}</h1>;
-}
 
-const foodIlike = [{}]
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
-function App() {
-  return (
-    <div>
-      <h1>Hellllllo</h1>
-      <Food fav="pizza" />
-      <Food fav="hamburgur" />
-    </div>
-  );   
+  getMovies = async() => {
+    const {data: {data: {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false});
+  }
+
+  componentDidMount(){
+    this.getMovies();
+  }
+
+  render(){
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+           <div className="loader">
+              <span className="loader__text">Loading...</span>
+            </div> 
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+          <Movies 
+            key={movie.id} 
+            id={movie.id} 
+            year={movie.year} 
+            title={movie.title} 
+            summary={movie.summary} 
+            poster={movie.medium_cover_image}
+            genres={movie.genres}
+          />
+        ))}
+          </div>
+      )}
+    </section>
+    );
+  }
 }
 
 export default App;
